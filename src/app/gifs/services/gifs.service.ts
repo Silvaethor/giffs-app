37b81f8ec547db82cd780d522f5881cc86d18a5d
Marcos Barrado Zabalza <mbarradzab1@educacion.navarra.es>
2023-05-05 13:59:33 +0200

    Alamcenamiento local del historial

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,9 @@ export class GifsService {
   private serviceUrl:string = 'https://api.giphy.com/v1/gifs';
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+   }
 
 
   get tagsHistory() {
@@ -51,6 +53,20 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0,10);
+
+    this.saveLocalStorage();
   }
 
+  private saveLocalStorage():void {
+    localStorage.setItem('history',JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory= JSON.parse(localStorage.getItem('history')!);
+
+    if (this.tagsHistory[0])
+      this.searchTag(this.tagsHistory[0]);
+  }
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -12,8 +12,11 @@ export class SidebarComponent {
   constructor(private gifsService:GifsService){
 
   }
-  get tags(){
+  get tags():string[]{
     return this.gifsService.tagsHistory;
   }
 
+  searchTag(tag:string):void{
+    this.gifsService.searchTag(tag);
+  }
 }
