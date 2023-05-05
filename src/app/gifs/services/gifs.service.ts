3354c1561c39c8eee0d02b546c57fa2a6514f778
Marcos <silvaethor@gmail.com>
2023-05-05 12:48:00 +0200

    Commit inicial

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+const GIPHY_API_KEY = 'SjtJ19nP3h0uASD1IHG4szfr9S7fkKGR';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  public gifList: Gif[] = [];
+
+  private _tagsHistory: string[] = [];
+  private serviceUrl:string = 'https://api.giphy.com/v1/gifs';
+
+
+  constructor(private http: HttpClient) { }
+
+
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
+
+   searchTag(tag:string):void {
+    if(tag.length ===0) return;
+    this.organizeHistory(tag);
+
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=SjtJ19nP3h0uASD1IHG4szfr9S7fkKGR&q=valorant&limit=10')
+    // .then(resp => resp.json())
+    // .then(data => console.log(data));
+
+    const params = new HttpParams()
+    .set('api_key', GIPHY_API_KEY)
+    .set('limit','10')
+    .set('q',tag);
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{params})
+      .subscribe(resp => {
+        this.gifList = resp.data;
+
+      })
+
+  }
+
+
+
+  private organizeHistory(tag:string){
+    tag = tag.toLowerCase();
+
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter((oldtag) => oldtag !==tag);
+    }
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+  }
+
+}
